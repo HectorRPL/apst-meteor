f14a2d9bbba39b03be2f2feceddbac91331ffe8b
hEctor <hector.kaizen@gmail.com>
2018-05-24 15:47:03 -0500

    Se agregó el fontawesome

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import solid from '@fortawesome/fontawesome-free-solid';
+import brands from '@fortawesome/fontawesome-free-brands';
+import fontawesome from '@fortawesome/fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'app';
 
-  constructor() {}
+  constructor() {
+    fontawesome.library.add(solid, brands);
+  }
 
   ngOnInit() {
   }
